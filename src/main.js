f9acc68b1ee393d3859c59f9e0f1d7fa5bc2a55a
Tim Decker <tdecker@relatethenews.com>
2017-06-06 14:36:16 -0600

    --UPDATED file locations

---
+++ src/main.js
@@ -0,0 +1,17 @@
+/**
+ * Copyright 2017 INTERConnect Analytics, Inc. All Rights Reserved
+ * 
+ * Unauthorized use is prohibited
+ * All Rights Reserved
+ * LICENSE:    This software code is proprietary and for use or 
+ *             access only by Entities legally licensed by INTERConnect
+ *             Analytics, Inc.  It is illegal to reverse engineer
+ *             modify or use this code without prior written
+ *             permission.
+ * 
+ * @copyright 2017 INTERConnect Analytics, Inc. All Rights Reserved
+ * @author
+ * @version
+ */
+
+
